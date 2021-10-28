6167a5cecd94686d662726c0461384916d1c5117
amalary95 <Malary.Music@gmail.com>
2021-10-28 14:13:41 -0700

    saving progress

--- main_app/views.py
+++ main_app/views.py
@@ -7,18 +7,3 @@ class Home(View):
     def get(self, request):
         return HttpResponse('Yo yo')
 
-class  Signup(View):
-    def get(self,request):
-        form = UserCreationForm():
-        context = {"form":form}
-        return render(request,'registration/signup.html',context)
-
-    def post (self,request):
-        form = UserCreationFrom(request.POST)
-        if form.is_valid():
-            user= form.save()
-            login (request,user)
-            return redirect ('profile.html')
-        else:
-            context = {"form":form}
-            return render(request,"registrations/signup.html", context)

de7ea7da21e785ae5cad2514e286c38bef3a389b
amalary95 <Malary.Music@gmail.com>
2021-10-28 14:26:46 -0700

    trying to run sign up page

---
+++ main_app/views/user_views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm 
+
+class Signup(View):
+    # show a form to fill out
+    def get(self, request):
+        form = UserCreationForm()
+        context = {"form": form}
+        return render(request, "registration/signup.html", context)
+    # on form ssubmit validate the form and login the user.
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("artist_list")
+        else:
+            context = {"form": form}
+            return render(request, "registration/signup.html", context)

5f7ffc00561322fe02876ced1d5a2916f6f9ffdb
zparmenter <zparmenter20@gmail.com>
2021-10-28 19:47:39 -0500

    still working

--- main_app/models/user_model.py
+++ main_app/models/user_model.py
@@ -1,5 +1,7 @@
 from django.db import models 
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Profile(models.Model):
@@ -10,8 +12,11 @@ class Profile(models.Model):
     cur_city = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return(self.name)
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created: 
+        Profile.objects.create(user=instance)
 
-    class Meta:
-        ordering = ['name']
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, created, **kwargs):
+    instance.profile.save()

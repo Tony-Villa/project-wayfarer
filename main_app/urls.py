b4d910482c321ec76e872f478837294f9b5b565e
amalary95 <Malary.Music@gmail.com>
2021-10-28 17:11:57 -0700

    Fixes #9

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,6 +7,6 @@ from .views import *
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('accounts/signup/',views.Signup.as_view(),name= "signup"),
-    path('user/<int:PK>/profile', views.Profile.as_view(), name='profile'),
+    path('user/profile', views.Profile_View.as_view(), name='profile'),
 
     ]
--- main_app/views/user_views.py
+++ main_app/views/user_views.py
@@ -1,9 +1,12 @@
+from django.forms import models
 from django.shortcuts import render, redirect
 from django.views import View
 from django.http import HttpResponse
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.views.generic.base import TemplateView 
+from django.views.generic.base import TemplateView
+
+from main_app.models.user_model import Profile 
 class Signup(View):
     # show a form to fill out
     def get(self, request):
@@ -21,5 +24,14 @@ class Signup(View):
             context = {"form": form}
             return render(request, "registration/signup.html", context)
 
-class Profile(TemplateView):
+class Profile_View(TemplateView):
+    #get the users pk and pass it to the url's 
     template_name='user/profile.html'
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)
+
+        context['profile'] =  Profile.objects.filter(user = self.request.user)
+        
+        return context
+

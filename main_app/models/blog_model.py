c55d5db0ea6380b3279bece23242fcd5cba47b88
zparmenter <zparmenter20@gmail.com>
2021-10-30 18:27:36 -0500

    fixed comments model

--- main_app/models/blog_model.py
+++ main_app/models/blog_model.py
@@ -2,7 +2,6 @@ from django.db import models
 from .user_model import Profile
 from .rec_model import Reccomendation
 from .city_model import City
-from .comment_model import Comment
 from django.db.models.deletion import CASCADE
 
 class Blog(models.Model):
@@ -11,9 +10,18 @@ class Blog(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     img = models.CharField(max_length=1024, default='')
     city = models.ForeignKey(City, on_delete=CASCADE, related_name='cities')
-    comment = models.ForeignKey(Comment, on_delete=CASCADE, related_name='comments')
     profile = models.ManyToManyField(Profile)
     reccomendation = models.ManyToManyField(Reccomendation)
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    content = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    blog = models.ForeignKey(Blog, on_delete=CASCADE, related_name='blogs')
+    profile = models.ManyToManyField(Profile)
+
+    def __str__(self):
+        return self.created_at

f5ba6282e9bd7d7ca79c434ed73280971e488451
tonyv <tonyvilla77@gmail.com>
2021-11-03 22:36:11 -0700

    got random city view working

--- main_app/views/city_views.py
+++ main_app/views/city_views.py
@@ -35,7 +35,7 @@ class City_View(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['blogs'] = Blog.objects.all()
-        context['cities'] = City.objects.all().order_by('id')[:5]
+        context['cities'] = City.objects.all().order_by('?')[:5]
         return context
         
 

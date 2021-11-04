7343fa0a6eeee84d298fdd703d22b13d312eccab
zparmenter <zparmenter20@gmail.com>
2021-11-04 00:02:39 -0500

    blog default image

--- main_app/models/blog_model.py
+++ main_app/models/blog_model.py
@@ -9,7 +9,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=200, blank=False)
     post = models.TextField(max_length=4000, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    img = models.CharField(max_length=1024, default='')
+    img = models.CharField(max_length=1024, default='https://images.unsplash.com/photo-1601922047316-70884dbbde2f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y2hpY2FnbyUyMG5pZ2h0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60')
     city = models.ForeignKey(City, on_delete=CASCADE, related_name='cities')
     profile = models.ForeignKey(Profile, on_delete=CASCADE, related_name='profiles')
     reccomendation = models.ManyToManyField(Reccomendation)
